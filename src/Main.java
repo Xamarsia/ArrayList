0e24e3c900ceb967a01db661cdb5e1ba9c8c0146
xamarsia <xamarsia@gmail.com>
2022-12-11 21:52:05 +0100

    feat: arrayList implementation using Generics

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+//        int size = 6;
+//        ArrayList<Integer> array = new ArrayList<>(size);
+//        ArrayList<Integer> array1 = new ArrayList<Integer>(size);
+//        for (int i = 0; i < size; ++i) {
+//            array.add(i);
+//        }
+
+//        array.add(33);
+//        System.out.println(array.size());
+//        array.remove(2);
+//
+//        boolean a = array.contains(33);
+//        System.out.println(a);
+//
+//        for (int num : array) {
+//            System.out.println(num);
+//        }
+    }
+}
